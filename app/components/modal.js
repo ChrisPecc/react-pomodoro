c891aa2b14df3638e411319abb2d15a243de3a92
ChrisPecc <pecceu.christophe@gmail.com>
2020-09-14 11:06:06 +0200

    fix

--- app/components/modal.js
+++ app/components/modal.js
@@ -1,17 +1,17 @@
 import React from "react";
-import ResetTimer from "./reset-button";
+import ResetTimer from "./reset-timer";
 import CloseModal from "./close-modal";
 
 function ModalWindow(props) {
     return (
         <div>
             <h2>{"Your time is up!"}</h2>
-            <CloseModal
+            <CloseModal setIsModalActive={props.setIsModalActive} />
+            <ResetTimer
                 setIsModalActive={props.setIsModalActive}
                 setSeconds={props.setSeconds}
                 setMinutes={props.setMinutes}
             />
-            <ResetTimer setIsModalActive={props.setIsModalActive} />
         </div>
     );
 }
